0d24a0327b872eeae6adabc5806a23be12360071
sunxiaoou <sun_xiaoou@qq.com>
2018-06-21 11:19:04 +0800

    show experiences / projects

--- resume/reporter.py
+++ resume/reporter.py
@@ -18,39 +18,46 @@ class Reporter:
         return result
 
     @staticmethod
-    def str2p(str):
+    def name_html(name, file):
         if str is None:
             return None
-        return '<p>{}</p>'.format(str)
+        path = '/home/xixisun/suzy/resumes/html/jl'
+        return '<a href="file://{}/{}"title="resume">{}</a><br>'.format(path, file, name)
 
     @staticmethod
-    def age2p(birth):
+    def gender_html(gender):
+        if gender is None:
+            return None
+        return '{}<br>'.format(gender)
+
+    @staticmethod
+    def age_html(birth):
         if birth is None:
             return None
         today = datetime.today()
         age = today.year - birth.year - ((today.month, today.day) < (birth.month, birth.day))
-        return '<p>{}岁</p>'.format(age)
+        return '{}岁<br>'.format(age)
 
     @staticmethod
-    def spots2p(spots):
+    def spots_html(spots):
         if spots is None:
             return None
-        return '<p>{}</p>'.format(','.join(spots))
+        return '{}<br>'.format(','.join(spots))
 
     @staticmethod
-    def education2p(education):
+    def education_html(education):
         if education is None:
             return None
-        return '<p>{}</p>'.format(['大专', '本科', '硕士', 'MBA', 'EMBA', '博士',  '博士后'][education - 1])
+        return '{}<br>'.format(['大专', '本科', '硕士', 'MBA', 'EMBA', '博士',  '博士后'][education - 1])
 
     @staticmethod
-    def years2p(years):
+    def years_html(years):
         if years is None:
             return None
-        return '<p>{}年</p>'.format(years)
+        return '{}年<br>'.format(years)
 
     @staticmethod
-    def educations2p(educations):
+    def educations_html(educations):
         if educations is None:
             return None
         rank = educations.get(Keys.school_rank, 1)  # if None then 1
@@ -61,27 +68,27 @@ class Reporter:
         majors = educations.get(Keys.majors)
         degrees = educations.get(Keys.degrees)
         for i in range(len(schools)):
-            text += '<p>{}|{}|{}</p>'.format(schools[i], majors[i], degrees[i])
+            text += '{}|{}|{}<br>'.format(schools[i], majors[i], degrees[i])
         return text
 
     @staticmethod
-    def experiences2p(experiences):
+    def experiences_html(experiences):
         if experiences is None:
             return None
         a = experiences[0]
-        text = '<p>{} | {} | {} | {}</p>'.format(a.get(Keys.start_date), a.get(Keys.end_date), a.get(Keys.company),
-                                                 a.get(Keys.company_desc))
-        text += '<p>{}</p>'.format(a.get(Keys.job_desc))
-        return text if len(text) <= 100 else text[:100] + '...'
+        text = '{} | {} | {} | {}<br>'.format(a.get(Keys.start_date), a.get(Keys.end_date), a.get(Keys.company),
+                                              a.get(Keys.company_desc))
+        text += '{}<br>'.format(a.get(Keys.job_desc))
+        return text if len(text) <= 100 else text[:150] + '...'
 
     @staticmethod
-    def projects2p(projects):
+    def projects_html(projects):
         if projects is None:
             return None
         a = projects[0]
-        text = '<p>{} | {} | {}</p>'.format(a.get(Keys.start_date), a.get(Keys.end_date), a.get(Keys.project))
-        text += '<p>{}</p>'.format(a.get(Keys.project_desc))
-        text += '<p>{}</p>'.format(a.get(Keys.duty))
+        text = '{} | {} | {}<br>'.format(a.get(Keys.start_date), a.get(Keys.end_date), a.get(Keys.project))
+        text += '{}<br>'.format(a.get(Keys.project_desc))
+        text += '{}<br>'.format(a.get(Keys.duty))
         return text if len(text) <= 100 else text[:150] + '...'
 
     @staticmethod
@@ -90,8 +97,8 @@ class Reporter:
 <html>
     <head>
         <meta charset="utf-8">
-        <style type="text/css">td, th { border: 1px solid black; }</style>
-        <title>Testing Tony's Travels</title>
+        <style type="text/css">td, th {{ border: 1px solid black; }}</style>
+        <title>Found {}, show {}</title>
     </head>
     <body>
         <table>
@@ -104,7 +111,7 @@ class Reporter:
 
         tr = '''
             <tr>
-                <td>{}{}{}{}{}{}</td>
+                <td>{}{}{}{}{}{}{}</td>
                 <td>{}</td>
                 <td>{}</td>
                 <td>{}</td>
@@ -118,19 +125,23 @@ class Reporter:
 
         output_file = 'result.html'
         html = open(output_file, 'w')
-        html.write(head)
+        num = len(documents)
+        show_num = min(100, len(documents))
+        html.write(head.format(num, show_num))
 
-        for document in documents:
-            name = Reporter.str2p(document.get(Keys.name))
-            gender = Reporter.str2p(document.get(Keys.gender))
-            age = Reporter.age2p(document.get(Keys.birth))
-            spots = Reporter.spots2p(document.get(Keys.spots))
-            education = Reporter.education2p(document.get(Keys.education))
-            years = Reporter.years2p(document.get(Keys.years))
-            educations = Reporter.educations2p(document.get(Keys.educations))
-            experiences = Reporter.experiences2p(document.get(Keys.experiences))
-            projects = Reporter.projects2p(document.get(Keys.projects))
-            html.write(tr.format(name, gender, age, education, years, spots, educations, experiences, projects))
+        for i in range(show_num):
+            document = documents[i]
+            no = '{:02d}<br>'.format(i)
+            name = Reporter.name_html(document.get(Keys.name), document.get(Keys.file))
+            gender = Reporter.gender_html(document.get(Keys.gender))
+            age = Reporter.age_html(document.get(Keys.birth))
+            spots = Reporter.spots_html(document.get(Keys.spots))
+            education = Reporter.education_html(document.get(Keys.education))
+            years = Reporter.years_html(document.get(Keys.years))
+            educations = Reporter.educations_html(document.get(Keys.educations))
+            experiences = Reporter.experiences_html(document.get(Keys.experiences))
+            projects = Reporter.projects_html(document.get(Keys.projects))
+            html.write(tr.format(no, name, gender, age, education, years, spots, educations, experiences, projects))
 
         html.write(tail)
         html.close()
