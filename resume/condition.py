1cc1ec2c1a457d3b904da3b5f85b03343fd2830f
sunxiaoou <sun_xiaoou@qq.com>
2018-08-12 11:48:41 +0800

    create conditions from a dictionary

--- resume/condition.py
+++ resume/condition.py
@@ -7,15 +7,32 @@ from resume import Keys
 
 
 class Condition:
-    name = spot = industry = field = company = skills = school = school_rank = major = None
-    education1 = education2 = year1 = year2 = age1 = age2 = None
-    experience_key = project_key = None
+    name = 'name'
+    spot = 'spot'
+    industry = 'industry'
+    field = 'field'
+    age1 = 'age1'
+    age2 = 'age2'
+    skills = 'skills'
+
+    education2 = 'education2'
+    education1 = 'education1'
+    school_rank = 'school_rank'
+    school = 'school'
+    major = 'major'
+
+    year1 = 'year1'
+    year2 = 'year2'
+    company = 'company'
+    experience_key = 'experience_key'
+    project_key = 'project_key'
 
     @staticmethod
     def input_string(title):
         print('请输入' + title + ':')
-        str = input()
-        return str if str else None
+        s = input()
+        # return s if str else None
+        return s
 
     @staticmethod
     def input_number(title):
@@ -23,7 +40,7 @@ class Condition:
             print('请输入' + title + ':')
             num = input()
             if not num:
-                return None
+                return ''
             if num.isdecimal():
                 return int(num)
             print('请输入数字')
@@ -37,148 +54,175 @@ class Condition:
 
     @staticmethod
     def input():
+        entries = {}
         try:
-            Condition.name = Condition.input_string('姓名')
-            Condition.spot = Condition.input_string('城市')
-            Condition.industry = Condition.input_string('行业')
-            Condition.field = Condition.input_string('职业')
-            Condition.company = Condition.input_string('（前）雇主')
-            Condition.skills = []
+            print('基本信息:')
+            entries[Condition.name] = Condition.input_string('姓名')
+            entries[Condition.spot] = Condition.input_string('期望地点')
+            entries[Condition.industry] = Condition.input_string('期望行业(关键字)')
+            entries[Condition.field] = Condition.input_string('期望职业(关键字)')
+            entries[Condition.age1] = ''
+            # Condition.input_number('年龄_岁及以上')
+            entries[Condition.age2] = Condition.input_number('年龄_岁以下')
+            skills = []
             for i in range(3):
                 skill = Condition.input_string('技能-' + str(i + 1))
-                if skill is None:
+                if not skill:
                     break
-                Condition.skills.append(skill.upper())
-            Condition.school_rank = Condition.input_number('毕业学校类别(1:一般 2:211 3:985)以上(含)')
-            Condition.school = Condition.input_string('学校')
-            Condition.major = Condition.input_string('专业')
-            Condition.education1 = Condition.input_number('学历(1:大专 2:本科 3:硕士 4:MBA 5:EMBA 6:博士 7:博士后)以上(含)')
-            # Condition.education2 = Condition.input_number('学历(1:大专 2:本科 3:硕士 4:MBA 5:EMBA 6:博士 7:博士后)以下')
-            Condition.year1 = Condition.input_number('工作经验_年以上(含)')
-            # Condition.year2 = Condition.input_number('工作经验_年以下')
-            # Condition.age1 = Condition.input_number('年龄_岁以上(含)')
-            Condition.age2 = Condition.input_number('年龄_岁以下')
-            Condition.experience_key = Condition.input_string('工作经历(关键字)')
-            Condition.project_key = Condition.input_string('项目经历(关键字)')
+                skills.append(skill.upper())
+            entries[Condition.skills] = ', '.join(skills)
 
+            print('教育背景:')
+            entries[Condition.school_rank] = Condition.input_number('学校类别(1:一般 2:211 3:985)及以上')
+            entries[Condition.education1] =\
+                Condition.input_number('学历(1:大专 2:本科 3:硕士 4:MBA 5:EMBA 6:博士 7:博士后)及以上')
+            entries[Condition.education2] = ''
+            # Condition.input_number('学历(1:大专 2:本科 3:硕士 4:MBA 5:EMBA 6:博士 7:博士后)以下')
+            entries[Condition.school] = Condition.input_string('学校(关键字)')
+            entries[Condition.major] = Condition.input_string('专业(关键字)')
+
+            print('职业背景:')
+            entries[Condition.year1] = Condition.input_number('工作经验_年及以上')
+            entries[Condition.year2] = ''
+            # Condition.input_number('工作经验_年以下')
+            entries[Condition.company] = Condition.input_string('(前)雇主(关键字)')
+            entries[Condition.experience_key] = Condition.input_string('工作经历(关键字)')
+            entries[Condition.project_key] = Condition.input_string('项目经历(关键字)')
         except KeyboardInterrupt:
             Condition.interrupt()
+        return entries
 
+    """
     @staticmethod
     def __str__():
         return Condition.spot + ', ' + Condition.industry + ', ' + Condition.field + ', ' + Condition.major + ', ' +\
                Condition.school_rank + '\n' + Condition.education1 + '-' + Condition.education2 + ', ' +\
                Condition.year1 + '-' + Condition.year2 + ', ' + Condition.age1 + '-' + Condition.age2
+    """
 
     @staticmethod
     def range_int(num1, num2):
-        if num1 is None and num2 is None:
-            return None
+        if not num1 and not num2:
+            return ''
 
-        if num1 is not None and num1 == num2:
+        if num1 and num1 == num2:
             return num1
 
         conditions = {}
-        if num1 is not None:
+        if num1:
             conditions['$gte'] = num1
-        if num2 is not None:
+        if num2:
             conditions['$lt'] = num2
         return conditions
 
     @staticmethod
     def range_year(num1, num2):
-        if num1 is None and num2 is None:
-            return None
+        if not num1 and not num2:
+            return ''
 
         this_year = int(datetime.today().strftime('%Y'))
-        if num1 is not None and num1 == num2:
+        if num1 and num1 == num2:
             return this_year - num1
 
         conditions = {}
-        if num2 is not None:
+        if num2:
             conditions['$gte'] = this_year - num2
-        if num1 is not None:
+        if num1:
             conditions['$lt'] = this_year - num1
         return conditions
 
-
     @staticmethod
     def range_date(num1, num2):
-        if num1 is None and num2 is None:
-            return None
+        if not num1 and not num2:
+            return ''
 
         today = datetime.today()
-        if num1 is not None and num1 == num2:
+        if num1 and num1 == num2:
             return datetime(today.year - num1, today.month, today.day)
 
         conditions = {}
-        if num2 is not None:
+        if num2:
             conditions['$gte'] = datetime(today.year - num2, today.month, today.day)    # >= birth equals <= age
-        if num1 is not None:
+        if num1:
             conditions['$lt'] = datetime(today.year - num1, today.month, today.day)     # < birth equals > age
         return conditions
 
     @staticmethod
-    def create_conditions():
-        Condition.input()
-
+    def create_conditions(entries):
         conditions = {}
-        if Condition.name is not None:
-            conditions[Keys.name] = Condition.name
 
-        if Condition.spot is not None:
-            conditions[Keys.spots] = Condition.spot
+        # basic information
+        name = entries.get(Condition.name)
+        if name:
+            conditions[Keys.name] = name
 
-        if Condition.industry is not None:
-            conditions[Keys.industries] = {'$regex': Condition.industry}
+        spot = entries.get(Condition.spot)
+        if spot:
+            conditions[Keys.spots] = spot
 
-        if Condition.field is not None:
-            conditions[Keys.fields] = {'$regex': Condition.field}
+        industry = entries.get(Condition.industry)
+        if industry:
+            conditions[Keys.industries] = {'$regex': industry}
 
-        if Condition.company is not None:
-            conditions[Keys.companies] = {'$regex': Condition.company}
+        field = entries.get(Condition.field)
+        if field:
+            conditions[Keys.fields] = {'$regex': field}
 
-        if Condition.skills:
+        c = Condition.range_date(entries.get(Condition.age1), entries.get(Condition.age2))
+        if c:
+            conditions[Keys.birth] = c
+
+        skills = entries.get(Condition.skills)
+        if skills:
             """
             conditions['$or'] = [{Keys.skills + '.' + Keys.skill_level2: Condition.skill},
                                  {Keys.skills + '.' + Keys.skill_level3: Condition.skill}]
             conditions[Keys.skills] = {'$regex': Condition.skill}
             """
-            conditions[Keys.skills] = {'$all': Condition.skills}
+            conditions[Keys.skills] = {'$all': skills.split(',')}
 
-        # if Condition.school_rank is not None:
-            # conditions[Keys.educations + '.' + Keys.school_rank] = Condition.school_rank
-        c = Condition.range_int(Condition.school_rank, None)
-        if c is not None:
-            conditions[Keys.educations + '.' + Keys.school_rank] = c
-
-        if Condition.school is not None:
-            conditions[Keys.educations + '.' + Keys.schools] = {'$regex': Condition.school}
-
-        if Condition.major is not None:
-            conditions[Keys.educations + '.' + Keys.majors] = {'$regex': Condition.major}
-
-        c = Condition.range_int(Condition.education1, Condition.education2)
-        if c is not None:
+        # education background
+        c = Condition.range_int(entries.get(Condition.education1), entries.get(Condition.education2))
+        if c:
             conditions[Keys.education] = c
 
-        c = Condition.range_year(Condition.year1, Condition.year2)
-        if c is not None:
+        c = Condition.range_int(entries.get(Condition.school_rank), '')
+        if c:
+            conditions[Keys.educations + '.' + Keys.school_rank] = c
+
+        school = entries.get(Condition.school)
+        if school:
+            conditions[Keys.educations + '.' + Keys.schools] = {'$regex': school}
+
+        major = entries.get(Condition.major)
+        if major:
+            conditions[Keys.educations + '.' + Keys.majors] = {'$regex': major}
+
+        # career background
+        c = Condition.range_year(entries.get(Condition.year1), entries.get(Condition.year2))
+        if c:
             conditions[Keys.year] = c
 
-        c = Condition.range_date(Condition.age1, Condition.age2)
-        if c is not None:
-            conditions[Keys.birth] = c
+        company = entries.get(Condition.company)
+        if company:
+            conditions[Keys.companies] = {'$regex': company}
 
-        if Condition.experience_key is not None:
-            conditions[Keys.experiences] = {'$regex': Condition.experience_key}
+        experience_key = entries.get(Condition.experience_key)
+        if experience_key:
+            conditions[Keys.experiences] = {'$regex': experience_key}
 
-        if Condition.project_key is not None:
-            conditions[Keys.projects] = {'$regex': Condition.project_key}
+        project_key = entries.get(Condition.project_key)
+        if project_key:
+            conditions[Keys.projects] = {'$regex': project_key}
 
         pprint(conditions)
         return conditions
 
 
+def main():
+    entries = Condition.input()
+    Condition.create_conditions(entries)
+
+
 if __name__ == "__main__":
-    Condition.create_conditions()
+    main()
