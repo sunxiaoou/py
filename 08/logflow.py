82023c8c43ab9240e42a7edfd4c2b7cd217ca428
sunxiaoou <sun_xiaoou@qq.com>
2019-09-23 13:08:19 +0800

    split hexdump

--- 08/logflow.py
+++ 08/logflow.py
@@ -1,5 +1,7 @@
 #! /usr/local/bin/python3
 
+# ! /opt/python/bin/python3
+
 import re
 
 
@@ -8,13 +10,19 @@ def main():
     regex = re.compile(r'{|}')
     r2 = re.compile(r'\[.*\] ')
     r3 = re.compile(r'(\w+)')
+    r4 = re.compile(r'\\n')
     for line in log.readlines():
         line = line.rstrip()
         if regex.search(line) is not None:
             line = r2.sub('', line)
             words = line.split(': ')
             line = r3.sub(r'{0}:\1'.format(words[0]), words[1])
-        print(line)
+            print(line)
+        elif r4.search(line) is not None:
+            for i in line.split('\\n'):
+                print(i)
+        else:
+            print(line)
 
 
 if __name__ == "__main__":
