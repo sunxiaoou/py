7ffe8f1529b1da96a9cccada37a7b71b7faf2d04
sunxiaoou <sun_xiaoou@qq.com>
2020-05-12 08:04:07 +0800

    add questions

--- tmp/detective.py
+++ tmp/detective.py
@@ -1,6 +1,40 @@
 #! /usr/local/bin/python3
 from typing import List
 
+"""
+2018年刑侦科推理试题 单项选择，（每题10分，共100分）
+
+1. 这道题的答案是：
+    A.A B.B C.C D.D
+
+2. 第5髓的答案是；
+    A.C B.D C.A D.B
+
+3. 以下选项中哪一题的签案与其他三项不同；
+    A.第3题 B.第6姬 C.第2题 D.第4题
+
+4. 以下选项中哪两题的答案相同：
+    A.第1,5题 B.第2,7题 C.第1,9题 D.第6,10题
+
+5. 以下选项中哪一题的答案与本题相同：
+    A.第8题 B.第4题 C.第9题 D.第7题
+
+6. 以下选项中感两题的答案与第8题相问：
+    A.第2,4题 B.第1,6题 C.第3,10题 D.第5,9题
+
+7. 在此十道题中，被选中次数最少的选项字母为：
+    A.C B.B C.A D.D
+
+8. 以下选项中哪一题的答案与第1题的答案在字母中不相邻
+    A.第7题 B.第5题 C.第2题 D.第10题
+
+9. 已知“第1题与第6题的答案相同”与“第X题与第5题的答案相间”的真假性相反，那么X为：
+    A.第6题 B.第10题 C.第2题 D.第9题
+
+10. 在此10道题中，ABCD四个字母出现次数最多与最少者的差为：
+    A.3 B.2 C.4 D.1
+"""
+
 
 def check(a: List) -> bool:
     c2 = [a[2] == 0 and a[5] == 2,
@@ -86,7 +120,7 @@ def main():
             ans.pop()
 
     m, n = 4, 10
-    ans = [-1]                  # -1 is a placeholder, as answer begin with index 1
+    ans = [-1]                  # -1 is a placeholder, as answer begins with index 1
     backtrack()
     ans = [chr(ans[i] + ord('A')) for i in range(1, n + 1)]
     print(ans)
@@ -94,3 +128,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
