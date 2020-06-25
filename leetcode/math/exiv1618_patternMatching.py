11c976ccb73bafe878c04c8604a0de3e25c4ae5f
sunxiaoou <sun_xiaoou@qq.com>
2020-06-25 21:36:14 +0800

    initial

---
+++ leetcode/math/exiv1618_patternMatching.py
@@ -0,0 +1,46 @@
+#! /usr/local/bin/python3
+
+
+def patternMatching(pattern: str, value: str) -> bool:
+    if not pattern:
+        return not value                # v "" matches p ""
+    if not value:
+        return len(pattern) == 1        # v "" matches p "a" or "b"
+    m, n = len(pattern), len(value)
+    ca = pattern.count("a")             # count of a
+    cb = m - ca                         # count of b
+    if not ca or not cb:
+        return value[: n // m] * m == value     # summary of parts equal entirety
+    for i in range(n // ca):                    # try different i - length of a
+        j, r = divmod(n - i * ca, cb)
+        if r:                                   # not integer, illegal length of b
+            continue                            # skip
+        sa, sb = set(), set()
+        k = 0
+        for ch in pattern:                      # split value to a and b
+            if ch == 'a':
+                sa.add(value[k: k + i])
+                k += i
+            else:
+                sb.add(value[k: k + j])
+                k += j
+        if len(sa) == len(sb) == 1:             # all a are same, all b are same
+            return True
+    return False
+
+
+def main():
+    p = "bbbbbbbbbbbbbbabbbbb"
+    v = "ppppppppppppppjsftcleifftfthiehjiheyqkhjfkyfckbtwbelfcgihlrfkrwireflijkjyppppp"
+    print(patternMatching(p, v))                        # True
+    print(patternMatching("b", ""))                     # True
+    print(patternMatching("", "x"))                     # False
+    print(patternMatching("aabab", "catcatgocatgo"))    # True
+    print(patternMatching("aaaa", "dogdogdogdog"))      # False
+    print(patternMatching("abba", "dogcatcatdog"))      # True
+    print(patternMatching("abba", "dogcatcatfish"))     # False
+    print(patternMatching("abba", "dogdogdogdog"))      # True
+
+
+if __name__ == "__main__":
+    main()
