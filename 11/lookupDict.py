48cbc7dabd6f0d4b8c1539a920457e3cff5bbdd0
sunxiaoou <sun_xiaoou@qq.com>
2021-01-02 09:50:07 +0800

    initial

--- 11/lookupDict.py
+++ 11/lookupDict.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 import os
 import sys
+import time
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,23 +12,29 @@ def lookup(word: str):
     res = requests.get(url)
     res.raise_for_status()
     soup = BeautifulSoup(res.text, 'html.parser')
-    print("生词：" + word)
-    pronunciation = soup.find("ul", class_="Mean_symbols__5dQX7").getText()
-    print("发音：" + pronunciation)
-    paraphrase = soup.find("ul", class_="Mean_part__1RA2V").getText()
-    print("释义: " + paraphrase)
+    try:
+        pronunciation = soup.find("ul", class_="Mean_symbols__5dQX7").getText()
+        paraphrase = soup.find("ul", class_="Mean_part__1RA2V").getText()
+    except AttributeError:
+        print("{} Cannot be found".format(word))
+        return
+    print("{}   {}  {}".format(word, pronunciation, paraphrase))
+    # print("生词：" + word)
+    # print("发音：" + pronunciation)
+    # print("释义: " + paraphrase)
 
 
 def main():
     if len(sys.argv) < 2:
         print('Usage: {} vocabulary'.format(sys.argv[0]))
         sys.exit(1)
-    # sys.stdout.flush()
+    sys.stdout.flush()
     if os.path.isfile(sys.argv[1]):
         with open(sys.argv[1]) as f:
             for w in f.read().splitlines():
                 if w:
                     lookup(w)
+                    time.sleep(0.1)
     else:
         lookup(sys.argv[1])
 
---
+++ tmp/selectStocks.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+import sys
+import random
+
+
+def get_stocks(quantity: int):
+    list1 = ['AAPL', 'ADBE', 'AMZN', 'GOOGL', 'GS', 'KO', 'MSFT', 'NFLX', 'NVDA']
+    # list2 = ['IEF', 'QQQ', 'SPY']
+    list2 = ['SPY', 'ARKK', 'AAPL', 'ADBE', 'GS', 'KO', 'MSFT']
+    # print(sorted(the_list))
+    print(random.sample(list1, quantity))
+    print(random.choice(list2))
+
+
+def main():
+    n = len(sys.argv)
+    if n < 2:
+        print('Usage {} quantity'.format(sys.argv[0]))
+        sys.exit(1)
+    # sys.stdout.flush()
+    get_stocks(int(sys.argv[1]))
+
+
+if __name__ == "__main__":
+    main()
