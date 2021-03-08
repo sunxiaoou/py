cd02c1fba5af3af619b2408a2e58d3b5f11cfabf
sunxiaoou <sun_xiaoou@qq.com>
2021-03-08 20:16:11 +0800

    initial

--- finance/ts_prices.py
+++ finance/ts_prices.py
@@ -1,16 +1,17 @@
 #! /usr/bin/python3
+import sys
 
 import tushare as ts
 
 
 def close_prices(date: str) -> list:
-    codes = ['600009.SH', '600104.SH', '600036.SH', '600276.SH', '600309.SH', '600519.SH',
+    codes = ['600009.SH', '600036.SH', '600104.SH', '600276.SH', '600309.SH', '600519.SH',
              '600585.SH', '600660.SH', '600887.SH', '600900.SH', '601318.SH', '601901.SH',
              '603288.SH', '603886.SH',
              '000002.SZ', '000333.SZ', '000651.SZ', '000858.SZ', '000895.SZ', '002271.SZ',
              '002304.SZ', '002372.SZ', '002415.SZ', '002508.SZ', '002677.SZ']
 
-    pro = ts.pro_api('c88f69544442a2b334556f33c12c502c3a9aa28c60cf8ecbcb1c3e56')
+    pro = ts.pro_api('')    # needs ts.set_token() in advance
     prices = []
     for c in codes:
         df = pro.daily(ts_code=c, start_date=date, end_date=date)
@@ -19,7 +20,10 @@ def close_prices(date: str) -> list:
 
 
 def main():
-    prices = close_prices('20210305')
+    if len(sys.argv) < 2:
+        print("Usage: {} YYYYMMDD".format(sys.argv[0]))
+        sys.exit(1)
+    prices = close_prices(sys.argv[1])
     for p in prices:
         print(p)
 
