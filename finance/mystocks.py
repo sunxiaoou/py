eaaf5f7017020a442c342bd18625366d45bfc611
sunxiaoou <sun_xiaoou@qq.com>
2021-03-20 11:35:37 +0800

    merge zhaohang

--- finance/mystocks.py
+++ finance/mystocks.py
@@ -1,11 +1,4 @@
 #! /usr/bin/python3
-# cp -p ~/Desktop/"`ls -lrt ~/Desktop/*.png | tail -1 | awk -F/ '{print $NF}'`" $name.png
-# tesseract $name.png $name -l eng+chi_sim --psm 6; cat $name.txt
-# mystocks.py --date=210312 yh yh_2323.88.png 2323.88
-# mystocks.py --currency=hkd --exchange_rate=0.8384 --date=210312 hs hs_83088.38.png 83088.38
-# mystocks.py --currency=usd --exchange_rate=6.5081 --date=210312 hs hs_17379.64.png 17379.64
-
-
 import csv
 import getopt
 import re
@@ -20,44 +13,13 @@ from PIL import Image
 from save_to import save_to_spreadsheet, save_to_mongo
 
 
-Stocks = {
-    # A stocks
-    '000858': ('五粮液', 3),
-    '501046': ('财通福鑫', 3),
-    '512170': ('医疗ETF', 2),
-    '515170': ('食品饮料', 2),
-    '600009': ('上海机场', 3),
-    '600036': ('招商银行', 3),
-    '600309': ('万华化学', 3),
-    # HK stocks
-    '00388': ('香港交易所', 3),
-    '00700': ('腾讯控股', 3),
-    '02840': ('SPDR金ETF', 2),
-    '03033': ('南方恒生科技', 2),
-    '03690': ('美团-W', 3),
-    '07200': ('FL二南方恒指', 2),
-    '09988': ('阿里巴巴-SW', 3),
-    # US stocks
-    'AAPL': ('苹果', 3),
-    'AMZN': ('亚马逊', 3),
-    'ARKG': ('ARK Genomic ETF', 3),
-    'ARKK': ('ARK Innovation ETF', 3),
-    'ARKW': ('ARK Web x.0 ETF', 3),
-    'BABA': ('阿里巴巴', 3),
-    'BILI': ('哔哩哔哩', 3),
-    'GS':  ('高盛', 3),
-    'MSFT': ('微软', 3),
-    'PDD': ('拼多多', 3),
-    'SPY': ('标普500指数ETF', 2)}
-
-
 def usage_exit():
     print('Usage: {} --currency="rmb||hkd|usd" --exchange_rate=float --date=%y%m%d '
-          '"yh||hs|ft" png|csv balance'.format(sys.argv[0]))
+          '"zsb|yh||hs|ft" png|csv balance'.format(sys.argv[0]))
     sys.exit(1)
 
 
-def get_options() -> dict:
+def get_options(platforms: list) -> dict:
     opts = args = []
     try:
         opts, args = getopt.getopt(sys.argv[1:], '', ['currency=', 'exchange_rate=', 'date='])
@@ -67,7 +29,7 @@ def get_options() -> dict:
     if len(args) < 3:
         usage_exit()
 
-    if args[0] not in ['yh', 'hs', 'ft']:
+    if args[0] not in platforms:
         usage_exit()
     dic = {'platform': args[0],
            'datafile': args[1],
@@ -125,8 +87,22 @@ def get_distance(source: str, target: str) -> int:
     return grid[i][j]
 
 
-def get_closest_code(code: str) -> str:
-    return max(Stocks.keys(), key=lambda x: get_distance(code, x))
+def get_closest_code(code: str, codes: list) -> str:
+    return max(codes, key=lambda x: get_distance(code, x))
+
+
+def fill_values(platform: str, currency: str, exchange_rate: float, date: datetime, result: list):
+    for dic in result:
+        dic['platform'] = platform
+        dic['currency'] = currency
+        dic['exchange_rate'] = exchange_rate
+        dic['date'] = date
+        if currency == 'rmb':
+            dic['mv_rmb'] = dic['market_value']
+            dic['hg_rmb'] = dic['hold_gain']
+        else:
+            dic['mv_rmb'] = round(dic['market_value'] * exchange_rate, 2)
+            dic['hg_rmb'] = round(dic['hold_gain'] * exchange_rate, 2)
 
 
 def verify(result: list):
@@ -138,63 +114,102 @@ def verify(result: list):
                 print('hold_gain failed {} {}'.format(i, (i['nav'] - i['cost']) * i['volume']))
 
 
-def yinhe(image_file: str, cash: float, currency: str, exchange_rate: float, date: datetime) -> list:
+def zhaoshang_bank(image_file: str, cash: float, currency: str, exchange_rate: float, date: datetime) -> list:
+    result = [{'name': '现金', 'risk': 0, 'market_value': cash, 'hold_gain': 0}]
     text = recognize_image(image_file)
-    # print(text)
+    text = re.sub('[,‘]', '', text)
+    amounts = re.findall(r'[-+]?\d*\.\d+', text)
+    # print(amounts)
+    amounts = [float(i) for i in amounts]
+    result += [
+        {'name': '招赢尊享日日盈', 'risk': 0, 'market_value': amounts[1], 'hold_gain': amounts[2]},
+        {'name': '招赢尊享日日盈', 'risk': 0, 'market_value': amounts[3], 'hold_gain': amounts[4]},
+        {'name': '睿远平衡二十七期', 'risk': 1, 'market_value': amounts[5], 'hold_gain': amounts[6]},
+        {'name': '卓远一年半定开8号', 'risk': 1, 'market_value': amounts[7], 'hold_gain': amounts[8]}]
+    fill_values('招商银行', currency, exchange_rate, date, result)
+    # verify result
+    summary = sum([i['market_value'] for i in result[1:]])
+    assert amounts[0] == summary, print("summary({}) != {}".format(summary, amounts[0]))
+    return result
+
+
+def yinhe(image_file: str, cash: float, currency: str, exchange_rate: float, date: datetime) -> list:
+    stocks = {
+        '000858': ('五粮液', 3),
+        '501046': ('财通福鑫', 3),
+        '512170': ('医疗ETF', 2),
+        '515170': ('食品饮料', 2),
+        '600009': ('上海机场', 3),
+        '600036': ('招商银行', 3),
+        '600309': ('万华化学', 3)
+    }
+
     dic = {
-        'platform': '银河',
-        'currency': currency,
-        'exchange_rate': exchange_rate,
         'code': 'cash',
-        'date': date,
         'name': '现金',
         'risk': 0,
         'market_value': cash,
         'hold_gain': 0,
-        'mv_rmb': cash,
-        'hg_rmb': 0,
         'nav': 1}
     result = [dic.copy()]
+
+    text = recognize_image(image_file)
+    # print(text)
     for line in text.split('\n'):
         if line:
             line = re.sub('[,‘]', '', line)
             items = re.findall('[-+]?\d*\.?\d+', line)
             # print(items)
             if len(items) == 8:
-                if items[0] not in Stocks:
-                    items[0] = get_closest_code(items[0])
+                if items[0] not in stocks:
+                    items[0] = get_closest_code(items[0], list(stocks.keys()))
                 dic = {
-                    'platform': '银河',
-                    'currency': currency,
-                    'exchange_rate': exchange_rate,
-                    'date': date,
                     'code': items[0],
-                    'name': Stocks[items[0]][0],
-                    'risk': Stocks[items[0]][1],
+                    'name': stocks[items[0]][0],
+                    'risk': stocks[items[0]][1],
                     'volume': int(items[1]),
                     'market_value': my_float(items[5], -2),
                     'hold_gain': my_float(items[2], -2),
-                    'mv_rmb': my_float(items[5], -2),
-                    'hg_rmb': my_float(items[2], -2),
                     'nav': my_float(items[6], -3),
                     'cost': my_float(items[7], -3)}
                 result.append(dic.copy())
+    fill_values('银河', currency, exchange_rate, date, result)
+    assert len(result) == len(stocks) + 1, print("result({}) != stocks({})".format(len(result), len(stocks) + 1))
+    verify(result)
     return result
 
 
 def huasheng(image_file: str, cash: float, currency: str, exchange_rate: float, date: datetime) -> list:
+    hk_stocks = {
+        '00388': ('香港交易所', 3),
+        '00700': ('腾讯控股', 3),
+        '02840': ('SPDR金ETF', 2),
+        '03033': ('南方恒生科技', 2),
+        '03690': ('美团-W', 3),
+        '07200': ('FL二南方恒指', 2),
+        '09988': ('阿里巴巴-SW', 3),
+    }
+    us_stocks = {
+        'AAPL': ('苹果', 3),
+        'AMZN': ('亚马逊', 3),
+        'ARKG': ('ARK Genomic ETF', 3),
+        'ARKK': ('ARK Innovation ETF', 3),
+        'ARKW': ('ARK Web x.0 ETF', 3),
+        'BABA': ('阿里巴巴', 3),
+        'BILI': ('哔哩哔哩', 3),
+        'GS':  ('高盛', 3),
+        'MSFT': ('微软', 3),
+        'PDD': ('拼多多', 3),
+        'SPY': ('标普500指数ETF', 2)
+    }
+    stocks = hk_stocks if currency == 'hkd' else us_stocks
+
     dic = {
-        'platform': '华盛' + currency.upper(),
-        'currency': currency,
-        'exchange_rate': exchange_rate,
-        'date': date,
         'code': 'cash',
         'name': '现金',
         'risk': 0,
         'hold_gain': 0,
         'market_value': cash,
-        'mv_rmb': round(cash * exchange_rate, 2),
-        'hg_rmb': 0,
         'nav': 1}
     result = [dic.copy()]
 
@@ -207,41 +222,32 @@ def huasheng(image_file: str, cash: float, currency: str, exchange_rate: float, 
             # print(items)
             if re.search(r'\w{2,4}', items[0]):
                 items[0] = items[0].split()[0].upper()
-                if items[0] in Stocks:
+                if items[0] in stocks:
                     if len(items) == 9:         # remove digits in US stock's name
                         items.pop(1)
                     dic = {
-                        'platform': '华盛' + currency.upper(),
-                        'currency': currency,
-                        'exchange_rate': exchange_rate,
-                        'date': date,
                         'code': items[0],
-                        'name': Stocks[items[0]][0],
-                        'risk': Stocks[items[0]][1],
+                        'name': stocks[items[0]][0],
+                        'risk': stocks[items[0]][1],
                         'volume': int(items[1]),
                         'market_value': float(items[7]),
                         'hold_gain': float(items[3]),
-                        'mv_rmb': round(float(items[7]) * exchange_rate, 2),
-                        'hg_rmb': round(float(items[3]) * exchange_rate, 2),
                         'cost': float(items[5]),
                         'nav': float(items[6])}
                     result.append(dic.copy())
+    fill_values('华盛' + currency.upper(), currency, exchange_rate, date, result)
+    assert len(result) == len(stocks) + 1, print("result({}) != stocks({})".format(len(result), len(stocks) + 1))
+    verify(result)
     return result
 
 
 def futu(csv_file: str, cash: float, currency: str, exchange_rate: float, date: datetime) -> list:
     dic = {
-        'platform': '富途' + currency.upper(),
-        'currency': currency,
-        'exchange_rate': exchange_rate,
-        'date': date,
         'code': 'cash',
         'name': '现金',
         'risk': 0,
         'hold_gain': 0,
         'market_value': cash,
-        'mv_rmb': round(cash * exchange_rate, 2),
-        'hg_rmb': 0,
         'nav': 1}
     result = [dic.copy()]
     try:
@@ -251,41 +257,37 @@ def futu(csv_file: str, cash: float, currency: str, exchange_rate: float, date: 
             rows.pop(0)
             for row in rows:
                 dic = {
-                    'platform': '富途' + currency.upper(),
-                    'currency': currency,
-                    'exchange_rate': exchange_rate,
-                    'date': date,
                     'code': row[1],
                     'name': row[2],
                     'risk': 3,
                     'volume': int(row[3].split('@')[0]),
                     'market_value': my_float(row[5], -2),
                     'hold_gain': my_float(row[7], -2),
-                    'mv_rmb': round(my_float(row[5], -2) * exchange_rate, 2),
-                    'hg_rmb': round(my_float(row[7], -2) * exchange_rate, 2),
                     'cost': my_float(row[6], -4),
                     'nav': my_float(row[3].split('@')[1], -4)}
                 result.append(dic.copy())
     except FileNotFoundError as e:
         print(e)
 
+    fill_values('富途' + currency.upper(), currency, exchange_rate, date, result)
+    verify(result)
     return result
 
 
 def main():
-    platforms = {'yh': yinhe, 'hs': huasheng, 'ft': futu}
-    options = get_options()
+    platforms = {'zsb': zhaoshang_bank, 'yh': yinhe, 'hs': huasheng, 'ft': futu}
+    options = get_options(list(platforms.keys()))
 
     result = platforms[options['platform']](options['datafile'],
                                             options['cash'],
                                             options['currency'],
                                             options['exchange_rate'],
                                             datetime.strptime(options['date'], '%y%m%d'))
-    verify(result)
+    # verify(result)
     # pprint(result)
     print(len(result))
 
-    save_to_spreadsheet('finance.xlsx', options['date'], result)
+    # save_to_spreadsheet('finance.xlsx', options['date'], result)
     # save_to_mongo('mystocks', result)
 
 
