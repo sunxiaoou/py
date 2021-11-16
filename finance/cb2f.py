eea440f2973f380e8fb4b0c5ff7131be03812bf9
sunxiaoou <sun_xiaoou@qq.com>
2021-11-16 09:26:48 +0800

    initial

---
+++ finance/cb2f.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+import sys
+
+import pyperclip
+
+
+def clipboard2file(file: str):
+    text = pyperclip.paste()
+    if file == 'auth/ths_cookie.txt':
+        header = 'Cookie: '
+        assert text.startswith(header), print(text[: len(header)])
+        text = text.lstrip(header)
+    with open(file) as fp:
+        f = open(file, 'w')
+        f.write(text)
+
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: {} txt'.format(sys.argv[0]))
+        sys.exit(1)
+
+    clipboard2file(sys.argv[1])
+
+
+if __name__ == "__main__":
+    main()
