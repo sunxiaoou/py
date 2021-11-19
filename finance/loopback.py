459dcbbfa80b5c3fd694d88a9a5f87c3faf77e25
sunxiaoou <sun_xiaoou@qq.com>
2021-11-19 12:27:18 +0800

    add funds

--- finance/loopback.py
+++ finance/loopback.py
@@ -1,32 +1,91 @@
 #! /usr/bin/python3
+import re
 import sys
 from datetime import date
 from datetime import datetime
 
 import matplotlib.pyplot as plt
+import numpy
 import openpyxl
 import pandas as pd
 import akshare as ak
 from openpyxl import load_workbook
 
+names = {
+    "sz161039": "1000增强LOF",
+    "sz164906": "中概互联网LOF",
+    "sh501009": "生物科技LOF",
+    "sh501050": "50AHLOF",
+    "sh501090": "消费龙头LOF",
+    "sh502000": "500增强LOF",
+    "sh510310": "沪深300ETF易方达",
+    "sh510580": "中证500ETF易方达",
+    "sh510710": "上证50ETF博时",
+    "sh512000": "券商ETF",
+    "sh512170": "医疗ETF",
+    "sh512260": "中证500低波ETF",
+    "sh512800": "银行ETF",
+    "sh513050": "中国互联网ETF",
+    "sh515180": "红利ETF易方达",
 
-def to_excel(xlsx: str, sheet: str, df: pd.DataFrame):
-    try:
-        wb = load_workbook(xlsx)
-    except FileNotFoundError:
-        wb = openpyxl.Workbook()
+    "004868": "交银股息优化",
+    "000595": "嘉实泰和",
+    "001766": "上投摩根医疗健康",
+    "001810": "中欧潜力价值",
+    "001974": "景顺长城量化新动力",
+    "003095": "中欧医疗健康A",
+    "005259": "建信龙头企业",
+    "005267": "嘉实价值精选",
+    "005354": "富国沪港深行业精选A",
+    "006228": "中欧医疗创新A",
+    "110011": "易方达优质精选",
+    "161005": "富国天惠成长",
+    "163402": "兴全趋势投资",
+    "163406": "兴全合润",
+    "163415": "兴全商业模式优选",
+    "166002": "中欧新蓝筹A",
+    "169101": "东方红睿丰",
+    "377240": "上投摩根新兴动力",
+    "519035": "富国天博创新",
+    "519688": "交银精选",
+    "540003": "汇丰晋信动态策略A",
 
-    writer = pd.ExcelWriter(xlsx, engine='openpyxl')
-    writer.book = wb
-    writer.sheets = {worksheet.title: worksheet for worksheet in wb.worksheets}
-    df.to_excel(writer, sheet_name=sheet, index=False)
-    writer.save()
+    "000248": "汇添富中证消费ETF联接",
+    "001556": "天弘中证500A",
+    "001594": "天弘中证银行A",
+    "003318": "景顺长城中证500低波动",
+    "004069": "南方中证全指证券",
+    "006327": "易方达中证海外50ETF联接",
+    "090010": "大成中证红利指数A",
+    "110003": "易方达上证50指数A",
+    "163407": "兴全沪深300A",
+    "164906": "交银中证海外中国互联网",
+    "501009": "汇添富中证生物科技",
+    "501050": "华夏上证50AH",
+    "501090": "华宝中证消费龙头",
+    "519671": "银河沪深300价值",
+    "540012": "汇丰晋信恒生A股龙头"
+}
+
+
+def get_daily(code: str, begin: date) -> pd.DataFrame:
+    if re.match(r'(sh|sz)\d{6}', code) is not None:
+        df = ak.stock_zh_index_daily(symbol=code)[['date', 'close']]
+    elif re.match(r'\d{6}', code) is not None:
+        df = ak.fund_em_open_fund_info(fund=code, indicator='累计净值走势')   # [['净值日期', '单位净值']]
+        df = df.rename({'净值日期': 'date', '累计净值': 'close'}, axis=1)
+        if df.dtypes['close'] != numpy.dtype('float64'):
+            df['close'] = df['close'].apply(lambda x: float(x))
+    else:
+        assert True
+    df = df[df['date'] > begin]
+    df['index'] = df['date']
+    df = df.set_index('index')
+    return df
 
 
 def get_index_name(code: str) -> str:
     df = ak.stock_zh_index_spot()
-    print(df)
-    to_excel('loopback.xlsx', 'indexes', df)
     i = df.loc[df['代码'] == code].index[0]
     return df.at[i, '名称']
 
@@ -52,46 +111,39 @@ def xirr(df: pd.DataFrame, date_column: str, amount_column: str) -> float:
     return guess - 1
 
 
+def to_excel(xlsx: str, sheet: str, df: pd.DataFrame):
+    try:
+        wb = load_workbook(xlsx)
+    except FileNotFoundError:
+        wb = openpyxl.Workbook()
+
+    writer = pd.ExcelWriter(xlsx, engine='openpyxl')
+    writer.book = wb
+    writer.sheets = {worksheet.title: worksheet for worksheet in wb.worksheets}
+    df.to_excel(writer, sheet_name=sheet, index=False)
+    writer.save()
+
+
 def loop_back(code: str, begin: date):
-    names = {
-        "sz161039": "1000增强LOF",
-        "sz164906": "中概互联网LOF",
-        "sh501009": "生物科技LOF",
-        "sh501050": "50AHLOF",
-        "sh501090": "消费龙头LOF",
-        "sh502000": "500增强LOF",
-        "sh510310": "沪深300ETF易方达",
-        "sh510580": "中证500ETF易方达",
-        "sh510710": "上证50ETF博时",
-        "sh512000": "券商ETF",
-        "sh512170": "医疗ETF",
-        "sh512260": "中证500低波ETF",
-        "sh512800": "银行ETF",
-        "sh513050": "中国互联网ETF",
-        "sh515180": "红利ETF易方达"
-    }
-    name = names[code]
-    df = ak.stock_zh_index_daily(symbol=code)[['date', 'close']]
-    df = df[df['date'] > begin]
-    df['index'] = df['date']
-    df = df.set_index('index')
-    df['每期金额'] = 100
-    df['累计金额'] = df['每期金额'].cumsum()
+    df = get_daily(code, begin)
+    df['每期定投金额'] = 100
+    df['累计定投金额'] = df['每期定投金额'].cumsum()
     fee_rate = 0.001
-    df['每期数量'] = df['每期金额'] / df['close'] * (1 - fee_rate)
-    df['累计数量'] = df['每期数量'].cumsum()
-    df['累计净值'] = df['close'] * df['累计数量']
+    df['每期持仓数量'] = df['每期定投金额'] / df['close'] * (1 - fee_rate)
+    df['累计持仓数量'] = df['每期持仓数量'].cumsum()
+    df['累计持仓净值'] = df['close'] * df['累计持仓数量']
     df['现金流'] = -100
     m = df.shape[0]     # number of row
-    cumulative_amount = df.iloc[m - 1, df.columns.get_loc('累计金额')]
-    cumulative_net = df.iloc[m - 1, df.columns.get_loc('累计净值')]
+    cumulative_amount = df.iloc[m - 1, df.columns.get_loc('累计定投金额')]
+    cumulative_net = df.iloc[m - 1, df.columns.get_loc('累计持仓净值')]
     hold_gain = cumulative_net - cumulative_amount
     df.iloc[m - 1, df.columns.get_loc('现金流')] += cumulative_net
     print(df)
     return_rate = xirr(df, 'date', '现金流')
     print(cumulative_amount, cumulative_net, hold_gain, return_rate)
     # to_excel('loopback.xlsx', symbol, df)
-    df[['累计金额', '累计净值']].plot(figsize=(12, 6), grid=True, title=name + '定投曲线')
+    title = '{}({})_定投曲线'.format(names[code], code)
+    df[['累计定投金额', '累计持仓净值']].plot(figsize=(12, 6), grid=True, title=title)
 
 
 def main():
@@ -99,10 +151,8 @@ def main():
         print('Usage: {} code YYYYmmdd'.format(sys.argv[0]))
         sys.exit(1)
 
-    loop_back(sys.argv[1], datetime.strptime(sys.argv[2], '%Y%m%d').date())
-    # loop_back('sh510310', date(2015, 6, 1))           # 沪深300ETF易方达
-    # loop_back('sh501050', date(2015, 6, 1))           # 50AHLOF
-    # loop_back('sh502000', date(2015, 6, 1))           # 500增强
+    begin = datetime.strptime(sys.argv[2], '%Y%m%d').date()
+    loop_back(sys.argv[1], begin)
     plt.show()
 
 
