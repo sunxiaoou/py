e0be336cda901be6154a9c0ceb562e7215d81b28
sunxiaoou <sun_xiaoou@qq.com>
2021-12-09 14:59:52 +0800

    input date

--- portfolio/screw.py
+++ portfolio/screw.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 import os
 import re
+import sys
+from datetime import datetime
 from time import sleep
 from urllib.parse import quote
 
@@ -25,25 +27,30 @@ def save_pic(date: str, url: str):
         print(e, 'none123')
 
 
-def zsxq():
+def request_zsxq(url: str, headers: dict) -> str:
+    response = requests.get(url, headers=headers)
+    i = 0
+    while i < 10 and not response.json().get('succeeded'):
+        sleep(0.1)
+        response = requests.get(url, headers=headers)
+        i += 1
+    assert response.status_code == 200, print('status_code({}) != 200'.format(response.status_code))
+    items = response.json()['resp_data']['topics']
+    for i in items:
+        create = i['create_time']
+        try:
+            pic = i['talk']['images'][1]['original']['url']
+        except KeyError:
+            pic = i['talk']['images'][2]['original']['url']
+        save_pic(create, pic)
+    return create
+
+
+def main():
     with open('zsxq_cookie.txt', 'r') as f:
         cookie = f.read()[:-1]      # delete last '\n'
-    url_base = 'https://api.zsxq.com/v2/hashtags/2425888411/topics?count=20'
     headers = {
         'Cookie': cookie,
-        # 'Host': url.split('/')[2],
-        # 'Origin': 'https://wx.zsxq.com',
-        # 'Referer': 'https://wx.zsxq.com/',
-        # 'Sec-Fetch-Dest': 'empty',
-        # 'Sec-Fetch-Mode': 'cors',
-        # 'Sec-Fetch-Site': 'same-site',
-        # 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) '
-        #               'AppleWebKit/537.36 (KHTML, like Gecko) '
-        #               'Chrome/88.0.4324.192 Safari/537.36'
-        # # X-Request-Id': '284bd31d4-75f1-1242-228a-eeb2ae40681',
-        # X-Signature': '70395b502fd7e1944843823176f1688be3e038d2',
-        # X-Timestamp': '1638756631',
-        # X-Version': '2.14.0'
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
         'Connection': 'keep-alive',
@@ -56,32 +63,17 @@ def zsxq():
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0'
     }
 
+    url_base = 'https://api.zsxq.com/v2/hashtags/2425888411/topics?count=20'
     end_time = ''
-    # for j in range(2):
-    while True:
-        print(end_time)
+    if len(sys.argv) > 1:
+        date = datetime.strptime(sys.argv[1], '%Y%m%d')
+        end_time = date.strftime('%Y-%m-%dT%H:%M:%S.%f')[: -3] + '+0800'
+
+    for i in range(2):
+        # print(end_time)
         url = url_base + '&end_time=' + quote(end_time) if end_time else url_base
-        response = requests.get(url, headers=headers)
-        i = 0
-        while i < 10 and not response.json().get('succeeded'):
-            sleep(0.1)
-            response = requests.get(url, headers=headers)
-            i += 1
-        assert response.status_code == 200, print('status_code({}) != 200'.format(response.status_code))
-        items = response.json()['resp_data']['topics']
-        for i in items:
-            # id = i['topic_id']
-            create = i['create_time']
-            pic = i['talk']['images'][1]['original']['url']
-            # print(create, pic)
-            save_pic(create, pic)
-        end_time = create
-    # return df
-
-
-def main():
-    # print(get_cookies())
-    zsxq()
+        print(url)
+        end_time = request_zsxq(url, headers)
 
 
 if __name__ == "__main__":
