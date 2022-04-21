7aa4fde684f8c8d82d499c82308ab92bc918afcb
sunxo <sun_xiaoou@qq.com>
2022-04-21 15:42:59 +0800

    initial

--- atbswp/12/ppjson.py
+++ atbswp/12/ppjson.py
@@ -2,18 +2,23 @@
 import json
 import pprint
 import re
+import sys
 
 import pyperclip
 
 
-def ppjson():
-    s = pyperclip.paste()
-    s = re.sub('\n', '', s)
-    pprint.pprint(json.loads(s))
-
-
 def main():
-    ppjson()
+    if len(sys.argv) < 2:
+        s = pyperclip.paste()
+        s = re.sub('\n', '', s)
+        pprint.pprint((json.loads(s)))
+    else:
+        with open(sys.argv[1]) as f:
+            lines = f.readlines()
+            for i in range(len(lines)):
+                line = re.sub('\n', '', lines[i])
+                print(i)
+                pprint.pprint((json.loads(line)))
 
 
 if __name__ == "__main__":
