096e40106fd92b8aad3cbd20dbd53f3f01fd3eb7
sunxiaoou <sun_xiaoou@qq.com>
2022-10-21 16:50:20 +0800

    initial

---
+++ tmp/login_hue.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python3
+import json
+
+import requests
+
+
+def login_hue(username: str, password: str):
+    url = "http://localhost:8000/hue/accounts/login"
+    payload = {}
+    headers = {
+        'Test': 'application/json',
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    assert response.status_code == 200
+    dic = json.loads(response.text)
+    dic['sessionid'] = response.cookies.get('sessionid')
+    print(dic)
+
+    payload = {
+        'username': username,
+        'password': password,
+        'next': '/',
+        'csrfmiddlewaretoken': dic['csrfmiddlewaretoken']
+    }
+    files = []
+    headers = {
+        'Test': 'application/json',
+        'Cookie': 'csrftoken={}; sessionid={}'.format(dic['csrftoken'], dic['sessionid'])
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    assert response.status_code == 200
+    return json.loads(response.text)
+
+
+def main():
+    dic = login_hue('sun_xo', 'sun_xo')
+    print('csrftoken={}; sessionid={}'.format(dic['csrftoken'], dic['sessionid']))
+
+
+if __name__ == "__main__":
+    main()
