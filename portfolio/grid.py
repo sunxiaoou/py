67f1e665de7f309db2a46d30a35c4825733659c5
sunxiaoou <sun_xiaoou@qq.com>
2022-11-26 16:46:59 +0800

    add comments on picture

--- portfolio/grid.py
+++ portfolio/grid.py
@@ -12,6 +12,8 @@ class Grid:
         self.code = code
         self.interval = interval
         self.unit = unit
+        self.high = high
+        self.low = low
         self.number = (high - low) // interval
         self.index = 0
         self.benchmark = high
@@ -73,8 +75,8 @@ class Grid:
         data = data[['date', 'open']]
         data = data.rename({'open': self.code}, axis=1)
         data = data.dropna().set_index('date')
-        # print(date)
         data.index.name = None
+        # print(date)
 
         buy = trans[trans['opt'] == 1][['date', 'price']]
         buy = buy.dropna().set_index('date', drop=True)
@@ -84,12 +86,31 @@ class Grid:
         sell.index.name = None
 
         pyplot.figure(figsize=(10, 6))
+        pyplot.title('可转债网格回测 %s (%s ~ %s)' % (self.code, data.index[0], data.index[-1]))
+        pyplot.ylabel('开盘价(元)')
         pyplot.grid()
-        pyplot.gca().xaxis.set_major_locator(ticker.MultipleLocator(40))
+        pyplot.gca().xaxis.set_major_locator(ticker.MultipleLocator(data.shape[0] // 8))
         pyplot.xticks(rotation=30)
         pyplot.plot(data.index, data[self.code])
         pyplot.plot(buy.index, buy['price'], 'og')
         pyplot.plot(sell.index, sell['price'], 'or')
+
+        pyplot.figtext(0.9, 0.85, ' Grid')
+        pyplot.figtext(0.9, 0.80, ' 格数 %d格' % (self.number + 1))
+        pyplot.figtext(0.9, 0.75, ' 最高 %d元' % self.high)
+        pyplot.figtext(0.9, 0.70, ' 最低 %d元' % self.low)
+        pyplot.figtext(0.9, 0.65, ' 间隔 %d元' % self.interval)
+        pyplot.figtext(0.9, 0.60, ' 单位 %d张' % self.unit)
+        pyplot.figtext(0.9, 0.55, ' Trade')
+        pyplot.figtext(0.9, 0.50, ' 开盘买(绿)')
+        pyplot.figtext(0.9, 0.45, ' 开盘卖(红)')
+        pyplot.figtext(0.9, 0.40, ' Result')
+        pyplot.figtext(0.9, 0.35, ' 当前网格 %d' % self.index)
+        pyplot.figtext(0.9, 0.30, ' 基准 %.2f' % self.benchmark)
+        pyplot.figtext(0.9, 0.25, ' 市值 %.2f' % self.value)
+        pyplot.figtext(0.9, 0.20, ' 成本 %.2f' % self.cost)
+        pyplot.figtext(0.9, 0.15, ' 收益 %.2f' % (self.value - self.cost))
+        pyplot.figtext(0.9, 0.05, '- 同光和尘')
         pyplot.show()
 
     def trade_daily(self, data: pd.DataFrame):
