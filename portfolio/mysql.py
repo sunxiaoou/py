a403980f9bf82420e06a35f7352d44abdf279be1
sunxiaoou <sun_xiaoou@qq.com>
2023-01-01 22:12:15 +0800

    add testcase

--- portfolio/mysql.py
+++ portfolio/mysql.py
@@ -1,6 +1,8 @@
-#! /usr/local/bin/python3
+#! /usr/bin/python3
+import json
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import pandas as pd
 # import pymysql
 
@@ -9,25 +11,135 @@ pd.set_option('display.max_rows', 4000)
 
 
 class MySql:
-    def __init__(self, host='localhost', port=3306, user='manga', password='manga', database='finance'):
-        self.db = create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(user, password, host, port, database))
+    def __init__(self, host='localhost', port=3306, user='manga', password='manga', database='portfolio'):
+        self.db = create_engine('mysql+pymysql://%s:%s@%s:%s/%s' % (user, password, host, port, database))
 
     # def close(self):
     #     self.db.close()
 
+    def last_row(self, table: str, column: str) -> tuple:
+        query = 'SELECT * FROM %s ORDER BY %s DESC LIMIT 1' % (table, column)
+        return self.db.execute(query).fetchone()
+
+    def insert(self, table: str, row: dict):
+        session = sessionmaker(bind=self.db)()
+        val_str = ', '.join(["'{}'".format(x) if isinstance(x, str) else str(x) for x in row.values()])
+        stmt = 'INSERT INTO %s (%s) VALUES (%s)' % (table, ', '.join(row.keys()), val_str)
+        print(stmt)
+        session.execute(stmt)
+        session.commit()
+
     def to_frame(self, table: str, fields: list = None) -> pd.DataFrame:
         s = ','.join(fields) if fields else '*'
-        return pd.read_sql('select {} from {}'.format(s, table), self.db)
+        return pd.read_sql('select %s from %s' % (s, table), self.db)
 
     def from_frame(self, table: str, frame: pd.DataFrame):
         frame.to_sql(table, con=self.db, if_exists='append', index=False)
 
 
+def threshold() -> pd.DataFrame:
+    with open('threshold.json') as f:
+        data = json.load(f)
+    df = pd.DataFrame(data)
+    df['代码'] = df['代码'].apply(lambda x: 'C' + x if x.isnumeric() else x)
+    df = df.rename({'_id': 'name', '代码': 'code', '参考指标': 'reference', '最低': 'lowest', '低估': 'low',
+                    '高估': 'high', '最高': 'highest', '场内代码': 'onsite', '场外代码': 'offsite'}, axis=1)
+    # print(df.columns)
+    return df
+
+
+def valuation() -> pd.DataFrame:
+    dic = {
+        '_id': 'timestamp',
+        '上证180': 'C000010',
+        '上证红利': 'C000015',
+        '上证50': 'C000016',
+        '沪深300': 'C000300',
+        '科创50': 'C000688',
+        '环保行业': 'C000827',
+        '中证1000': 'C000852',
+        '中证100': 'C000903',
+        '500增强': 'C000905',
+        '中证800': 'C000906',
+        '300价值': 'C000919',
+        '中证红利': 'C000922',
+        '基本面50': 'C000925',
+        '中证消费': 'C000932',
+        '医药100': 'C000978',
+        '可选消费': 'C000989',
+        '深证成指': 'C399001',
+        '创业板': 'C399006',
+        '深红利': 'C399324',
+        '深证100': 'C399330',
+        '地产行业': 'C399393',
+        '央视50': 'C399550',
+        '基本面60': 'C399701',
+        '基本面120': 'C399702',
+        '中证养老': 'C399812',
+        '军工行业': 'C399967',
+        '证券行业': 'C399975',
+        '银行行业': 'C399986',
+        '中证医疗': 'C399989',
+        '基建行业': 'C399995',
+        '中证白酒': 'C399997',
+        'MSCI质量': 'C707717',
+        '食品饮料': 'C930653',
+        '家用电器': 'C930697',
+        '生物科技': 'C930743',
+        '500低波动': 'C930782',
+        '建筑材料': 'C931009',
+        '消费龙头': 'C931068',
+        '竞争力指数': 'C931142',
+        '科技100': 'C931187',
+        '沪港深消费50': 'C931357',
+        '50AH优选': 'C950090',
+        '红利机会': 'CSPSADRP',
+        '消费红利': 'H30094',
+        '中概互联': 'H30533',
+        'A股龙头': 'HSCAIT',
+        'H股指数': 'HSCEI',
+        '恒生指数': 'HSI',
+        '恒生科技': 'HSTECH',
+        '美股消费': 'IXY',
+        '纳斯达克100': 'NDX',
+        '标普科技': 'S5INFT',
+        '全球医疗': 'SPG120035',
+        '香港中小': 'SPHCMSHP',
+        '标普500': 'SPX',
+    }
+
+    with open('valuation.json') as f:
+        data = json.load(f)
+    val = pd.DataFrame(data)
+    val = val[dic.keys()]
+    val = val.rename(dic, axis=1)
+
+    with open('sh000985.json') as f:
+        data = json.load(f)
+    sh = pd.DataFrame(data)
+    sh = sh[['_id', 'close', 'star']]
+    sh = sh.rename({'_id': 'timestamp', 'close': 'sh000985'}, axis=1)
+
+    df = pd.merge(val, sh, on='timestamp')
+    df['timestamp'] //= 1000
+
+    return df
+
+
 def main():
-    db = MySql()
-    df = db.to_frame('instant_price')
+    # df = threshold()
+    # print(df['code'])
+    # df = valuation()
+    # print(df.columns)
+    # print(df)
+    # db = MySql(database='portfolio')
+    # db.from_frame('valuation', df)
+    # # df = db.to_frame('instant_price')
     # df = db.to_frame('instant_price', ['code', 'price'])
-    print(df)
+    # db = MySql(database='portfolio')
+    # print(db.last_row('valuation', 'timestamp'))
+    db = MySql(database='manga')
+    db.insert('export', {'export_id': 27, 'name': '费沙自治领'})
 
 
 if __name__ == "__main__":
---
+++ portfolio/mysql.sh
@@ -0,0 +1,102 @@
+#! /bin/sh
+
+createTable() {
+mysql -u $user -p$pass $db << EOF
+  DROP TABLE IF EXISTS $table;
+  SET character_set_client = utf8mb4;
+  CREATE TABLE $table (
+    timestamp INT NOT NULL,
+    C000010 FLOAT,
+    C000015 FLOAT,
+    C000016 FLOAT,
+    C000300 FLOAT,
+    C000688 FLOAT,
+    C000827 FLOAT,
+    C000852 FLOAT,
+    C000903 FLOAT,
+    C000905 FLOAT,
+    C000906 FLOAT,
+    C000919 FLOAT,
+    C000922 FLOAT,
+    C000925 FLOAT,
+    C000932 FLOAT,
+    C000978 FLOAT,
+    C000989 FLOAT,
+    C399001 FLOAT,
+    C399006 FLOAT,
+    C399324 FLOAT,
+    C399330 FLOAT,
+    C399393 FLOAT,
+    C399550 FLOAT,
+    C399701 FLOAT,
+    C399702 FLOAT,
+    C399812 FLOAT,
+    C399967 FLOAT,
+    C399975 FLOAT,
+    C399986 FLOAT,
+    C399989 FLOAT,
+    C399995 FLOAT,
+    C399997 FLOAT,
+    C707717 FLOAT,
+    C930653 FLOAT,
+    C930697 FLOAT,
+    C930743 FLOAT,
+    C930782 FLOAT,
+    C931009 FLOAT,
+    C931068 FLOAT,
+    C931142 FLOAT,
+    C931187 FLOAT,
+    C931357 FLOAT,
+    C950090 FLOAT,
+    CSPSADRP FLOAT,
+    H30094 FLOAT,
+    H30533 FLOAT,
+    HSCAIT FLOAT,
+    HSCEI FLOAT,
+    HSI FLOAT,
+    HSTECH FLOAT,
+    IXY FLOAT,
+    NDX FLOAT,
+    S5INFT FLOAT,
+    SPG120035 FLOAT,
+    SPHCMSHP FLOAT,
+    SPX FLOAT,
+    sh000985 FLOAT NOT NULL,
+    star FLOAT NOT NULL,
+    PRIMARY KEY (timestamp)
+  ) engine = innodb default charset = utf8mb4;
+EOF
+}
+
+insertTable() {
+mysql -u $user -p$pass $db << EOF
+  INSERT INTO $table (code, name, reference, lowest, low, high, highest, onsite, offsite)
+    VALUES ('C000906', '中证800', '市盈率', 9, 15, 19, 52, '515800', '010763');
+EOF
+}
+
+selectTable() {
+mysql -u $user -p$pass $db << EOF
+  # SELECT * FROM $table;
+  SELECT * FROM $table ORDER BY timestamp DESC LIMIT 1;
+  SELECT COUNT(*) FROM $table;
+EOF
+}
+
+
+## main ##
+
+if [ $# -lt 4 ]
+then
+    echo "$0 user pass db table"
+    exit 1
+fi
+
+user=$1
+pass=$2
+db=$3
+table=$4
+
+# createTable
+# insertTable
+selectTable
