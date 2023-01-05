827b5d21d08af1948943f9514bf1738dc8e68445
sunxiaoou <sun_xiaoou@qq.com>
2023-01-05 13:28:03 +0800

    add batch

--- portfolio/grid.py
+++ portfolio/grid.py
@@ -130,30 +130,70 @@ class Grid:
         pyplot.savefig('%s_%s.png' % (self.code, self.name), dpi=400, bbox_inches='tight')
         pyplot.show()
 
-    def trade_daily(self):
+    def trade_daily(self) -> str:
         for _, row in self.data.iterrows():
             # print(row['date'], row['open'])
             self.trade_open(row['date'], row['open'])
 
-        if sys.argv[6] == '0':
-            print('%s(%s) %s_%s %d %d %.2f %.2f %.2f %.2f' %
-                  (self.code, self.name, self.data.iloc[0]['date'], self.data.iloc[-1]['date'], len(self.trans),
-                   self.index * self.quantity, self.benchmark, self.value, self.cost, self.value - self.cost))
+        if len(sys.argv) < 7 or sys.argv[6] == '0':
+            return '%s(%s) %s_%s %d %d %.2f %.2f %.2f %.2f' \
+                   % (self.code, self.name, self.data.iloc[0]['date'], self.data.iloc[-1]['date'], len(self.trans),
+                      self.index * self.quantity, self.benchmark, self.value, self.cost, self.value - self.cost)
         else:
             trans = pd.DataFrame(self.trans)
             print(trans)
             print('%s~%s' % (self.data.iloc[0]['date'], self.data.iloc[-1]['date']), self)
             self.draw(self.data, trans)
+            return ''
+
+
+def get_codes(file: str) -> list:
+    with open(file) as f:
+        text = f.read()
+    blocks = text.split('代码')
+    lines1 = blocks[1].split('\n')
+    l1 = [row.split()[1] for row in lines1[1: -2]]
+    lines5 = blocks[5].split('\n')
+    l5 = [row.split()[1] for row in lines5[1: -2]]
+    lines6 = blocks[6].split('\n')
+    l6 = [row.split()[1] for row in lines6[1: -2]]
+    return sorted(list(set(l1 + l5 + l6)))
+
+
+def trade_codes(codes: list, low: float, high: float, change: float, quantity: int, start_date: str)\
+        -> pd.DataFrame:
+    result = []
+    for code in codes:
+        s = Grid(code, low, high, change, quantity, start_date).trade_daily()
+        s = s.split()
+        result.append([s[0], float(s[-1])])
+    return pd.DataFrame(result, columns=['name', '%d_%.1f' % (low, change)])
+
+
+def batch(file: str, start_date: str) -> pd.DataFrame:
+    codes = get_codes(file)
+    df1 = trade_codes(codes, 115, 135, 5, 80, start_date)
+    df2 = trade_codes(codes, 120, 150, 7.5, 80, start_date)
+    df3 = trade_codes(codes, 125, 165, 10, 80, start_date)
+    df = pd.merge(df1, df2, on='name')
+    df = pd.merge(df, df3, on='name')
+    df['max_col_name'] = df.iloc[:, -3:].idxmax(axis=1)
+    df['max_value'] = df[df.columns[-4: -1]].max(axis=1)
+    df = df.sort_values(by='max_value', ascending=False)
+    df.reset_index(drop=True, inplace=True)
+    return df
 
 
 def main():
     if len(sys.argv) > 7:
         grid = Grid(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), int(sys.argv[5]),
                     sys.argv[7])
-        grid.trade_daily()
+        print(grid.trade_daily())
     elif len(sys.argv) == 7:
         grid = Grid(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), int(sys.argv[5]))
-        grid.trade_daily()
+        print(grid.trade_daily())
+    elif len(sys.argv) == 3:
+        print(batch(sys.argv[1], sys.argv[2]))
     else:
         print('Usage: {} code low high change quantity 0|1 [start_date(%Y-%m-%d)]'.format(sys.argv[0]))
         sys.exit(1)
