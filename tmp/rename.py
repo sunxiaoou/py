45a752b6f424b556d7dc32ede8193b41441b2f16
sunxiaoou <sun_xiaoou@qq.com>
2023-01-26 11:05:21 +0800

    add extra argvment

--- tmp/rename.py
+++ tmp/rename.py
@@ -1,29 +1,40 @@
 #! /usr/bin/python3
 
 import os
+import re
 import sys
 
 
-def rename(dir: str, file: str, ext: str):
-    for i, filename in enumerate(sorted(os.listdir(dir))):
-        if filename.endswith(ext):
-            src = filename if dir == '.' else dir + filename
-            dst = '{}_{:02d}.{}'.format(file, i + 1, ext)
-            if dir != '.':
-                dst = dir + dst
-            print('mv "%s" %s' % (src, dst))
-            os.rename(src, dst)
+def rename(path: str, src: str, tgt: str, start_no: int):
+    os.chdir(path)
+    todo = []
+    done = []
+    for name in os.listdir('.'):
+        if re.match(src, name):
+            todo.append(name)
+        elif re.match(tgt, name):
+            done.append(name)
+    # print(todo)
+    # print(done)
+
+    for i, src_name in enumerate(sorted(todo)):
+        serial_no = '_%02d' % (i + start_no + len(done))
+        tgt_name = tgt.replace('_??', serial_no)
+        if not os.path.isfile(tgt_name):
+            print('mv "%s" %s' % (src_name, tgt_name))
+            if sys.argv[-1] == 'fire':
+                os.rename(src_name, tgt_name)
 
 
 def main():
     n = len(sys.argv)
     if n < 4:
-        print('Usage: ' + sys.argv[0] + ' directory/' + ' file' + ' extension')
+        print('Usage:   %s path src tgt [start_no] [fire]' % sys.argv[0])
+        print('example: %s summertime.*\\.mp4 summerTimeRender_??.mp4 6' % sys.argv[0])
         sys.exit(1)
-    # sys.stdout.flush()
-    rename(sys.argv[1], sys.argv[2], sys.argv[3])
+    start_no = 1 if n < 5 else int(sys.argv[4])
+    rename(sys.argv[1], sys.argv[2], sys.argv[3], start_no)
 
 
 if __name__ == "__main__":
     main()
-
