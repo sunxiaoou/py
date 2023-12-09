3fceedff45899bd3624ed9b76ac4306f171ed6c2
sunxiaoou <sun_xiaoou@qq.com>
2023-12-09 15:48:41 +0800

    add class

--- portfolio/market.py
+++ portfolio/market.py
@@ -12,118 +12,110 @@ from pymongo import MongoClient
 
 from mysql import MySql
 
-fund_base = 'https://fund.xueqiu.com/dj/open/fund/deriveds?'
 
-URL_LIST = 'https://stock.xueqiu.com/v5/stock/portfolio/stock/list.json?'
-URL_STOCK = 'https://stock.xueqiu.com/v5/stock/batch/quote.json?'
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) '
-                  'AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/88.0.4324.192 Safari/537.36',
-}
+class Market:
+    fund_base = 'https://fund.xueqiu.com/dj/open/fund/deriveds?'
 
-funds = [
-    '001556', '001594', '001810', '003318', '004069', '005259',
-    '005827', '006327', '008138', '008407', '070023', '090010',
-    '110003', '110011', '161005', '161128', '163402', '163407',
-    '164906', '166002', '260108', '262001', '270002', '501021',
-    '501050', '519671', '540003', '540006']
-
-mongo_host = '127.0.0.1'
-mongo_port = 27017
-mongo_db_name = 'finance'
-mongo_db_collection = 'xueqiu'
-
-
-def get_list(category: int, pid: int) -> list:
-    params = {
-        'size': 1000,
-        'category': category,
-        'pid': pid
+    URL_LIST = 'https://stock.xueqiu.com/v5/stock/portfolio/stock/list.json?'
+    URL_STOCK = 'https://stock.xueqiu.com/v5/stock/batch/quote.json?'
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/88.0.4324.192 Safari/537.36',
     }
-    url = URL_LIST + urlencode(params)
-    response = requests.request("GET", url, headers=HEADERS)
-    response.raise_for_status()
-    stocks = json.loads(response.text)['data']['stocks']
-    return [x['symbol'] for x in stocks]
+    with open('auth/xq_cookie.txt', 'r') as f:
+        HEADERS['Cookie'] = f.read()[:-1]       # delete last '\n'
 
-
-def get_stocks(codes: list) -> list:
-    HEADERS['Host'] = URL_STOCK.split('/')[2]
-    params = {
-        'symbol': ','.join(codes),
-        'extend': 'detail',
-        'is_delay_hk': 'true'
-    }
-    url = URL_STOCK + urlencode(params)
-    # print(url)
-    response = requests.get(url, headers=HEADERS)
-    response.raise_for_status()
-    result = []
-    items = response.json()['data']['items']
-    for i in items:
-        dic = {
-            'code': i['quote']['symbol'],
-            'ts': datetime.fromtimestamp(i['quote']['timestamp'] / 1000),
-            'name': i['quote']['name'],
-            'price': i['quote']['current'],
-            'pc': i['quote']['percent']}
-        result.append(dic.copy())
-    return result
-
-
-def get_cvtbones(codes: list) -> list:
-    HEADERS['Host'] = URL_STOCK.split('/')[2]
-    params = {
-        'symbol': ','.join(codes),
-        'extend': 'detail',
-        'is_delay_hk': 'true'
-    }
-    url = URL_STOCK + urlencode(params)
-    # print(url)
-    response = requests.get(url, headers=HEADERS)
-    response.raise_for_status()
-    result = []
-    items = response.json()['data']['items']
-    for i in items:
-        quote = i['quote']
-        dic = {
-            # 'ts': datetime.fromtimestamp(quote['timestamp'] / 1000),
-            'code': quote['symbol'],
-            'name': quote['name'],
-            'price': quote['current'],
-            'premium': quote['convert_bond_ratio'],
-            'remains': round(quote['outstanding_amt'] / quote['total_issue_scale'] * 100, 2),
-            'days': (datetime.fromtimestamp(quote['maturity_date'] / 1000).date() - date.today()).days,
-            'pc': quote['percent']
+    @staticmethod
+    def get_list(category: int, pid: int) -> list:
+        params = {
+            'size': 1000,
+            'category': category,
+            'pid': pid
         }
-        result.append(dic.copy())
-    return result
+        url = Market.URL_LIST + urlencode(params)
+        response = requests.request("GET", url, headers=Market.HEADERS)
+        response.raise_for_status()
+        stocks = json.loads(response.text)['data']['stocks']
+        return [x['symbol'] for x in stocks]
 
+    @staticmethod
+    def get_stocks(codes: list) -> list:
+        Market.HEADERS['Host'] = Market.URL_STOCK.split('/')[2]
+        params = {
+            'symbol': ','.join(codes),
+            'extend': 'detail',
+            'is_delay_hk': 'true'
+        }
+        url = Market.URL_STOCK + urlencode(params)
+        # print(url)
+        response = requests.get(url, headers=Market.HEADERS)
+        response.raise_for_status()
+        result = []
+        items = response.json()['data']['items']
+        for i in items:
+            dic = {
+                'code': i['quote']['symbol'],
+                'ts': datetime.fromtimestamp(i['quote']['timestamp'] / 1000),
+                'name': i['quote']['name'],
+                'price': i['quote']['current'],
+                'pc': i['quote']['percent']}
+            result.append(dic.copy())
+        return result
 
-def get_funds(base_url: str, codes: list) -> list:
-    HEADERS['Host'] = base_url.split('/')[2]
-    params = {
-        'codes': ','.join(codes),
-    }
-    url = base_url + urlencode(params)
-    # print(url)
-    try:
-        response = requests.get(url, headers=HEADERS)
-        if response.status_code == 200:
-            result = []
-            items = response.json()['data']
-            for i in items:
-                dic = {
-                    'code': i['fd_code'],
-                    'date': datetime.strptime(i['end_date'], '%Y-%m-%d'),
-                    'name': i['fd_name'],
-                    # 'price': Decimal128(i['unit_nav'])}
-                    'price': float(i['unit_nav'])}
-                result.append(dic.copy())
-            return result
-    except requests.ConnectionError as e:
-        print('Error', e.args)
+    @staticmethod
+    def get_cvtbones(codes: list) -> list:
+        Market.HEADERS['Host'] = Market.URL_STOCK.split('/')[2]
+        params = {
+            'symbol': ','.join(codes),
+            'extend': 'detail',
+            'is_delay_hk': 'true'
+        }
+        url = Market.URL_STOCK + urlencode(params)
+        # print(url)
+        response = requests.get(url, headers=Market.HEADERS)
+        response.raise_for_status()
+        result = []
+        items = response.json()['data']['items']
+        for i in items:
+            quote = i['quote']
+            dic = {
+                # 'ts': datetime.fromtimestamp(quote['timestamp'] / 1000),
+                'code': quote['symbol'],
+                'name': quote['name'],
+                'price': quote['current'],
+                'premium': quote['convert_bond_ratio'],
+                'remains': round(quote['outstanding_amt'] / quote['total_issue_scale'] * 100, 2),
+                'days': (datetime.fromtimestamp(quote['maturity_date'] / 1000).date() - date.today()).days,
+                'pc': quote['percent']
+            }
+            result.append(dic.copy())
+        return result
+
+    @staticmethod
+    def get_funds(base_url: str, codes: list) -> list:
+        Market.HEADERS['Host'] = base_url.split('/')[2]
+        params = {
+            'codes': ','.join(codes),
+        }
+        url = base_url + urlencode(params)
+        # print(url)
+        try:
+            response = requests.get(url, headers=Market.HEADERS)
+            if response.status_code == 200:
+                result = []
+                items = response.json()['data']
+                for i in items:
+                    dic = {
+                        'code': i['fd_code'],
+                        'date': datetime.strptime(i['end_date'], '%Y-%m-%d'),
+                        'name': i['fd_name'],
+                        # 'price': Decimal128(i['unit_nav'])}
+                        'price': float(i['unit_nav'])}
+                    result.append(dic.copy())
+                return result
+        except requests.ConnectionError as e:
+            print('Error', e.args)
 
 
 def main():
@@ -131,22 +123,27 @@ def main():
         print('Usage: {} "a|cvtb|hk|us|fund"'.format(sys.argv[0]))
         sys.exit(1)
 
-    with open('auth/xq_cookie.txt', 'r') as f:
-        HEADERS['Cookie'] = f.read()[:-1]       # delete last '\n'
+    # with open('auth/xq_cookie.txt', 'r') as f:
+    #     HEADERS['Cookie'] = f.read()[:-1]       # delete last '\n'
 
     if sys.argv[1] == 'a':
-        result = get_stocks(get_list(1, 0))
+        result = Market.get_stocks(Market.get_list(1, 0))
     elif sys.argv[1] == 'hk':
-        result = get_stocks(get_list(1, -7))
+        result = Market.get_stocks(Market.get_list(1, -7))
     elif sys.argv[1] == 'us':
-        result = get_stocks(get_list(1, -6))
+        result = Market.get_stocks(Market.get_list(1, -6))
     elif sys.argv[1] == 'fund':
-        result = get_funds(fund_base, funds)
+        funds = [
+            '001556', '001594', '001810', '003318', '004069', '005259',
+            '005827', '006327', '008138', '008407', '070023', '090010',
+            '110003', '110011', '161005', '161128', '163402', '163407',
+            '164906', '166002', '260108', '262001', '270002', '501021',
+            '501050', '519671', '540003', '540006']
+        result = Market.get_funds(Market.fund_base, funds)
     elif sys.argv[1] == 'cvtb':
-        # result = get_cvtbones(get_list(1, 8) + get_list(1, 11))
-        result = get_cvtbones(get_list(1, 8) + get_list(1, 12))
+        result = Market.get_cvtbones(Market.get_list(1, 8) + Market.get_list(1, 12))
     elif sys.argv[1] == 'misc':
-        result = get_cvtbones(get_list(1, 4))
+        result = Market.get_cvtbones(Market.get_list(1, 4))
     # elif sys.argv[1] == 'grid':
     #     result = get_cvtbones(get_list(1, 12))
     else:
@@ -155,11 +152,19 @@ def main():
 
     # print(len(result))
     df = pd.DataFrame(result)
-    print(df.sort_values(by='pc', ascending=False))
-    print('mean({})'.format(round(df['pc'].mean(), 3)))
+    if sys.argv[1] == 'cvtb':
+        df = df.sort_values(by='pc', ascending=False)
+    print(df)
+    if sys.argv[1] != 'fund':
+        print('mean({})'.format(round(df['pc'].mean(), 3)))
 
     # mysql = MySql()
     # mysql.from_frame('instant_price', df)
+
+    mongo_host = '127.0.0.1'
+    mongo_port = 27017
+    mongo_db_name = 'finance'
+    mongo_db_collection = 'xueqiu'
 
     # client = MongoClient(host=mongo_host, port=mongo_port)
     # db = client[mongo_db_name]
