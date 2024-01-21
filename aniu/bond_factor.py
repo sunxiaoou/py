c55e951fc30c92858cc5d380f993709101b51404
sunxiaoou <sun_xiaoou@qq.com>
2024-01-21 14:41:24 +0800

    split method

--- aniu/bond_factor.py
+++ aniu/bond_factor.py
@@ -23,6 +23,11 @@ class Factor(ABC):
         self.prices = self.get_factor(self.PRICE, True)
         self.changes = self.prices.pct_change().shift(-1)
         self.changes = self.changes.fillna(method='pad')
+        self.signal = self.get_signal()
+        self.codes = self.get_codes()
+        self.cum_prod = self.get_cum_prod()
+        self.roi = self.get_return_on_investment()
+        self.max_dd = self.get_max_draw_down()
 
     def get_factor(self, column_name: str, fill: bool = False) -> pd.DataFrame:
         df = self.data[['bond_code', 'trade_date', column_name]]
@@ -32,43 +37,48 @@ class Factor(ABC):
         df.index.name = None
         return df
 
-    @abstractmethod
-    def get_signal(self) -> pd.DataFrame:
-        pass
-
     # 取排名最小的N个标的
     def filter(self, factor: pd.DataFrame) -> pd.DataFrame:
         return factor.apply(lambda x: x.rank(method='first') <= self.quantity, axis=1)
 
+    @abstractmethod
+    def get_signal(self) -> pd.DataFrame:
+        pass
+
     # 取标的的代码
-    def get_codes(self, signal: pd.DataFrame) -> pd.DataFrame:
-        df = signal.iloc[range(0, len(signal), self.freq)]
+    def get_codes(self) -> pd.DataFrame:
+        df = self.signal.iloc[range(0, len(self.signal), self.freq)]
         return df.apply(lambda row: pd.Series(df.columns[row]), axis=1)
 
-    def calc_pnl(self, signal: pd.DataFrame) -> pd.Series:
-        df = signal.iloc[range(0, len(signal), self.freq)]
-        signal_freq = pd.DataFrame(index=signal.index)
+    # 计算每天累计收益
+    def get_cum_prod(self) -> pd.DataFrame:
+        df = self.signal.iloc[range(0, len(self.signal), self.freq)]
+        signal_freq = pd.DataFrame(index=self.signal.index)
         signal_freq = signal_freq.join(df)
         signal_freq = signal_freq.fillna(method='pad')
         gross_pnl = self.changes[signal_freq].sum(axis=1) / signal_freq.sum(axis=1)
         # print(gross_pnl)
         cost = abs(signal_freq.diff()).sum(axis=1) * self.cost / signal_freq.sum(axis=1)
         # print(cost)
-        return gross_pnl - cost
+        series = (gross_pnl - cost + 1).cumprod()
+        return pd.DataFrame({'date': series.index, 'value': series.values})
+
+    def get_return_on_investment(self) -> float:
+        return round(self.cum_prod.value[len(self.cum_prod) - 1] * 100 - 100, 2)
+
+    def get_max_draw_down(self) -> tuple:
+        self.cum_prod['top'] = self.cum_prod['value'].expanding().max()  # 计算当日前最高点
+        self.cum_prod['dd'] = self.cum_prod['value'] / self.cum_prod['top']  # 计算历史最高点到当日剩余
+        date, remains = tuple(self.cum_prod.sort_values(by=['dd']).iloc[0][['date', 'dd']])
+        return date, round((1 - remains) * 100, 2)
+
+    def plot(self):
+        self.cum_prod.plot(figsize=(8, 4), grid=True)
 
     def loop_back(self):
-        signal = self.get_signal()
-        # print(self.get_codes(signal))
-        pnl = self.calc_pnl(signal)
-        tp = (1 + pnl).cumprod()  # 计算每天累计收益
-        tp = pd.DataFrame({'date': tp.index, 'value': tp.values})  # 将日期和每天累计收益对应起来
-        # 计算当日前最高点
-        tp['max'] = tp['value'].expanding().max()
-        # 计算历史最高点到当日剩余
-        tp['d'] = tp['value'] / tp['max']
-        end_date, remains = tuple(tp.sort_values(by=['d']).iloc[0][['date', 'd']])
-        print('总收益:', round((100 * tp.value[len(tp) - 1] - 100), 2), '%')
-        print('最大回撤:', round((1 - remains) * 100, 2), '%')
+        # print(self.get_codes())
+        print('总收益: {}%'.format(self.roi))
+        print('最大回撤: {}% on {}'.format(self.max_dd[1], self.max_dd[0]))
 
 
 class LowPrice(Factor):
